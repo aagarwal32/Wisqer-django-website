42bfa2b92f0e6859c8240e373218622373c153d7
Arjun <arjunagw02@gmail.com>
2024-09-03 20:16:41 -0400

    added base.html for improved adaptability

--- polls/views.py
+++ polls/views.py
@@ -8,13 +8,15 @@ def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {
         "latest_question_list": latest_question_list,
+        "title": "Poll Questions",
     }
     return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question":question})
+    return render(request, "polls/detail.html", {"question":question,
+                                                 "title": "Question Info",})
 
 
 def results(request, question_id):
